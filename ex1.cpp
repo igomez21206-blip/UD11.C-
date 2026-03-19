0a3c8de1fc2a947d111fa5bdefd2dce9540bb336
igomez21206-blip <igomez21206@iesjoanramis.org>
2026-03-19 12:28:00 +0100

    soy izan g devora

---
+++ ex1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string nom, cognom;
+
+    cout << "Introdueix el nom: ";
+    cin >> nom;
+    cout << "Introdueix el cognom: ";
+    cin >> cognom;
+
+    cout << "Hola, " << nom << " " << cognom << endl;
+
+    return 0;
+}
